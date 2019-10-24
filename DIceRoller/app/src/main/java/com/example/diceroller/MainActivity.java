e6e33619fb627f6707574519b1705db822edbdc9
Ben <bendeguz.kasuba@gmail.com>
2019-10-24 15:50:21 +0100

    added D-ICEBREAKER

--- DIceRoller/app/src/main/java/com/example/diceroller/MainActivity.java
+++ DIceRoller/app/src/main/java/com/example/diceroller/MainActivity.java
@@ -21,6 +21,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     EditText userInput;
     static int score = 0;
+    int num = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,8 +69,7 @@ public class MainActivity extends AppCompatActivity {
         TextView tv = this.findViewById(R.id.rndNumDisplay);
         TextView grats = this.findViewById(R.id.congratsDisplay);
         TextView scoreCntr = this.findViewById(R.id.pointCounter);
-        Random r = new Random();
-        int num = r.nextInt(6)+1;
+        num = rndNum();
 
 
         tv.setText(Integer.toString(num));
@@ -92,5 +92,44 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
+    }
+    public void onButtonBreaker (View view ){
+        dicebrkr(num);
+    }
+    public void dicebrkr(int x ){
+        TextView qstn = this.findViewById(R.id.questionDisplay);
+        switch(x){
+            case 0:
+                qstn.setText("Roll the die first!!");
+                break;
+            case 1:
+                qstn.setText("If you could go anywhere in the world, where would you go?");
+                break;
+            case 2:
+                qstn.setText("If you were stranded on a desert island, what three things would you want to take with you?");
+                break;
+            case 3:
+                qstn.setText("If you could eat only one food for the rest of your life, what would that be?");
+                break;
+            case 4:
+                qstn.setText("If you won a million dollars, what is the first thing you would buy?");
+                break;
+            case 5:
+                qstn.setText("If you could spend the day with one fictional character, who would it be?");
+                break;
+            case 6:
+                qstn.setText("If you found a magic lantern and a genie gave you three wishes, what would you wish?");
+                break;
+
+
+
+
+        }
+
+    }
+    public int rndNum(){
+        Random r = new Random();
+        int num = r.nextInt(6)+1;
+        return num;
     }
 }
